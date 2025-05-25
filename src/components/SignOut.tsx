dd8f910cbaaa433b578d6799a5366c2b164652c1
michaelynm <michaelmarcelino55@gmail.com>
2025-05-25 22:08:49 +0700

    feat: add confirmation modal on logout button

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -5,28 +5,83 @@ import { PiSignOut } from "react-icons/pi";
 import { cn } from "../lib/utils";
 import { signOut } from "next-auth/react";
 
+import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { useState } from "react";
+
 const SignOut = ({ className }: { className?: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const handleSignOut = () => {
+    setIsOpen(false);
     signOut();
     router.push("/signin");
     toast.success("Sign out successfully!");
   };
 
-  return (
-    <button
-      onClick={handleSignOut}
-      className={cn(
-        "group flex w-full items-center gap-3 rounded-lg p-3 cursor-pointer data-[focus]:bg-white/10",
-        className
-      )}
-    >
-      <div className="w-7 h-7 bg-[#B4B4B440] rounded-full flex items-center justify-center">
-        <PiSignOut className="text-base" />
-      </div>
+  function open() {
+    setIsOpen(true);
+  }
 
-      <button type="submit">Sign Out</button>
-    </button>
+  function close() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <div className="hover:bg-white/10 rounded-lg">
+        <Button
+          onClick={open}
+          className="rounded-md p-3 text-sm font-medium text-white focus:not-data-focus:outline-none data-[focus]:bg-white/10 data-focus:outline-white data-hover:bg-black/30"
+        >
+          <div className="w-full gap-3 rounded-full flex items-center justify-center">
+            <div className="w-7 h-7 bg-[#B4B4B440] rounded-full flex items-center justify-center">
+              <PiSignOut className="text-base" />
+            </div>
+            <p className="text-base">Sign Out</p>
+          </div>
+        </Button>
+
+        <Dialog
+          open={isOpen}
+          as="div"
+          className="relative z-10 focus:outline-none"
+          onClose={close}
+        >
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <DialogPanel
+                transition
+                className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+              >
+                <DialogTitle
+                  as="h3"
+                  className="text-base/7 font-bold text-white"
+                >
+                  Are you sure want to sign out?
+                </DialogTitle>
+                <p className="mt-2 text-sm/6 text-white/50">
+                  You will be logged out from your account.
+                </p>
+                <div className="mt-4 flex items-center gap-3">
+                  <Button
+                    className="inline-flex items-center gap-2 rounded-md bg-red-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
+                    onClick={close}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
+                    onClick={handleSignOut}
+                  >
+                    Logout
+                  </Button>
+                </div>
+              </DialogPanel>
+            </div>
+          </div>
+        </Dialog>
+      </div>
+    </div>
   );
 };
 
